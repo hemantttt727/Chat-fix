c7538f8e9217571cf67720fc0d8cad15f0ebc37a
hemantttt727 <hemantsoni8005@gmail.com>
2025-06-26 11:31:02 +0530

    Create index.js
    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to Chat Fix 💜</h1>
+      <p>A Gen Z dating app made for real vibes.</p>
+    </div>
+  );
+}
